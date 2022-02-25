a42bd37ddf00bd6632410169d54fc7c4a4a9914e
Henry <henryhein.hein@gmail.com>
2022-02-25 00:11:29 -0800

    skeleton app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import CurrencyConverter from './CurrencyConverter';
+import ExchangeRates from './ExchangeRates';
+import { FaTwitter, FaLinkedin, FaGithub, FaGlobe, FaCopyright } from 'react-icons/fa';
+import './App.css';
 
 const NotFound = () => {
   return <h2>404 Not Found</h2>;
@@ -8,14 +12,31 @@ const NotFound = () => {
 const App = () => {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">Currency Converter</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Convert </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Rates</a>
+            </li>
+          </ul>
+        </div>
       </nav>
-      <Switch>
-        {/* <Route path="/" exact component={ } />
-        <Route path="/" component={ } /> */}
-        <Route component={NotFound} />
-      </Switch>
+      
+      <Route path="/" exact component={ CurrencyConverter } />
+      <Route path="/" component={ ExchangeRates } />
+      <Route component={NotFound} />
+      
+      <footer className="text-center text-lg-start bg-dark">
+        <p><FaTwitter /> <a target="_blank" href="https://twitter.com">www.twitter.com</a></p>
+        <p><FaLinkedin /> <a target="_blank" href="https://linkedin.com">www.linkedin.com</a></p>
+        <p><FaGithub /> <a target="_blank" href="https://www.github.com/henry-hein">www.github.com/henry-hein</a></p>
+        <p><FaGlobe /> <a target="_blank" href="https://www.google.com">www.currencyconverter.com</a></p>
+        <div className="mx-auto"><FaCopyright /> All rights reserved to Currency Converter Inc.</div>
+      </footer>
     </Router>
   );
 }
---
+++ src/CurrencyConverter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CurrencyConverter extends React.Component {
+  render() {
+    return ( <h4> Hello </h4>)
+  }
+}
+
+export default CurrencyConverter;
